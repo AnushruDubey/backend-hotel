e270b32d4a0dd41ebda4c76d64b1c227f9b99673
AnushruDubey <dubeyanukriti4@gmail.com>
2025-03-28 13:54:03 +0530

    commit

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div className="container">
+            <div className="banner">
+               <div className="left">ANUKRITI</div>
+               <div className="right">
+                <p>102,Rishabh Apartment,Ranisati</p>
+                <p>Open: 10:00 AM - 11:30 PM</p>
+                </div> 
+            </div>
+            <div className="banner">
+                <div className="left">
+                    <p>Developed by Anukriti Dubey</p>
+                </div>
+                <div className="right">
+                    <p>All Rights Reserved By Anukriti Dubey.</p>
+                </div>
+            </div>
+        </div>
+
+    </footer>
+  )
+}
+
+export default Footer
